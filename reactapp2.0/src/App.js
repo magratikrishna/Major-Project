7201ca493bfd27d9bc36074f1fdf1dbe5a056de6
bhantanasmit@gmail.com <bhantanasmit@gmail.com>
2019-12-26 10:53:20 +0545

    added universal theming

--- reactapp2.0/src/App.js
+++ reactapp2.0/src/App.js
@@ -4,14 +4,35 @@ import "./App.css";
 
 //material ui
 import Button from "@material-ui/core/Button";
+//material theming for better customization {view documentation for more}
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+//importing colors
+import purple from "@material-ui/core/colors/purple";
+import green from "@material-ui/core/colors/green";
+//creating universal themes for better customization
+const theme = createMuiTheme({
+  palette: {
+    primary: purple,
+    secondary: green
+  },
+  status: {
+    danger: "orange"
+  }
+});
 
 function App() {
   return (
-    <div className="App">
-      <Button variant="contained" color="primary">
-        Hello World
-      </Button>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <React.Fragment>
+        <CssBaseline />
+        <div className="App">
+          <Button variant="contained" color="primary">
+            Hello World!
+          </Button>
+        </div>
+      </React.Fragment>
+    </MuiThemeProvider>
   );
 }
 
